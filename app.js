52e26a37e714e1c392ee1458c6269f191570d9a1
Tim <Tim.mouse@gmail.com>
2017-04-27 20:42:09 +0800

    add quotes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var quotes = require ('./routes/quotes');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -17,13 +19,15 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use(users);
+app.use (quotes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
